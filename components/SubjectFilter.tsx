b2b8a84166d82bbb75ec57c78673a10d1c78141e
gpahwa10 <gauravpahwa2002@gmail.com>
2025-07-27 04:12:32 +0530

    companions creating,fetching and filtering via topic and subject added

---
+++ components/SubjectFilter.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { subjects } from "@/constants";
+
+const SubjectFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("subject") || "";
+
+  const [subject,setSubject] = useState(query);
+
+  useEffect(()=>{
+    let newUrl ="";
+    if(subject ==='all'){
+      newUrl = removeKeysFromUrlQuery({
+        params: searchParams.toString(),
+        keysToRemove:['subject']
+      })
+    }else{
+      newUrl = formUrlQuery({params:searchParams.toString(),key:'subject',value:subject})
+    }
+
+  router.push(newUrl,{scroll:false});
+
+  },[subject]);
+
+  return (
+  <div>
+    <Select onValueChange={setSubject} value={subject}>
+      <SelectTrigger className="input ca">
+        <SelectValue placeholder="Subject" className="input"/>
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="all">All Subjects</SelectItem>
+        {
+          subjects.map((subject)=>(
+            <SelectItem key={subject} value={subject} className="">{subject}</SelectItem>
+          ))
+        }
+      </SelectContent>
+    </Select>
+  </div>
+  )
+}
+
+export default SubjectFilter
